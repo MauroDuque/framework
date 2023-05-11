0be575bb228a9d95fd011355d2ed45452ce428b5
MauroDuque <mauricio.duque.eje@gmail.com>
2023-05-11 11:31:29 -0400

    Framework

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -7,6 +7,7 @@
 #include "GAS.h"
 #include "water_flow.h"
 #include "tmp_102.h"
+#include "temp_max.h"
 
 void init_sensors(){
     qwiic_setup();
@@ -26,72 +27,3 @@ void read_sensors() {
     // loop_water_flow();
     //loop_tmp();
 }
- 
-
-// {
-//   "code": "7C:DF:A1:55:B5:C6",
-//   "name": "Fieldgate",
-//   "description": "Fieldgate",
-//   "sensors": [
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x60",
-//       "name": "Temperature 1"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x61",
-//       "name": "Temperature 2"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x62",
-//       "name": "Temperature 3"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x63",
-//       "name": "Temperature 4"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x64",
-//       "name": "Temperature 5"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x65",
-//       "name": "Temperature 6"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x66",
-//       "name": "Temperature 7"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x67",
-//       "name": "Temperature 8"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x10",
-//       "name": "GAS_COUNTER"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x11",
-//       "name": "GAS_COUNTER"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x80",
-//       "name": "Pulse GAS"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x40",
-//       "name": "Current 0"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x41",
-//       "name": "Current 1"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x90",
-//       "name": "Current 0"
-//     },
-//     {
-//       "code": "7C:DF:A1:55:B5:C6&0x91",
-//       "name": "Current 1"
-//     }
-//   ]
-// }
